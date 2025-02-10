e51024d41119e14df345b3e5d944caa1c27851af
ryanumoso2003 <ryanumoso04@gmail.com>
2025-02-09 21:07:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const { TouchableNativeFeedbackBase } = require("react-native/types");
+
+//Declare and Use Variables
+let studentName = "Ryan Umoso";
+let studentAge = 21;
+let isEnrolled = true;
+
+console.log("Student Name: ", studentName);
+console.log("Student Age: ", studentAge);
+console.log("Is Enrolled: ", isEnrolled);
+
+//Demonstrate Data Types
+let stringExample = "My name is Ryan";
+let numberExample = "21"
+let booleanExample = true;
+let arrayExample = ["apple", "banana", "cherry"];
+let objectExample = { firstName: "Ryan", lastName: "Umoso" };
+
+console.log("String Example: ", stringExample);
+console.log("Number Example: ", numberExample);
+console.log("Boolean Example: ", booleanExample);
+console.log("Array Example: ", arrayExample);
+console.log("Object Example: ", objectExample);
+
+//Use Basic Operators
+let sum = 10 + 21;
+let difference = 23 - 12;
+let product = 12 * 3;
+let quotient = 36 / 6;
+
+console.log("The sum is: ", sum);
+console.log("The difference is: ", difference);
+console.log("The product is: ", product);
+console.log("The quotient is: ", quotient);
+
+let greaterThan = 12 > 3;
+let lessThan = 12 < 29;
+let equalTo = 9 === 9;
+let notEqualTo = 18 !== 45;
+
+console.log("Greater Than: ", greaterThan);
+console.log("Less Than: ", lessThan);
+console.log("Equal to: ", equalTo);
+console.log("Not Equal to: ", notEqualTo);
+
+let andOperator = true && false;
+let orOperator = true || false;
+let notOperator = !true;
+
+console.log("AND Operator: ", andOperator);
+console.log("OR Operator: ", orOperator);
+console.log("NOT Operator: ", notOperator);
